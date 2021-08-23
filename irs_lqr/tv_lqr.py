ab5f9d79cf0e566cfe428eee3cc8b158c5892664
hjsuh94 <hjsuh94@gmail.com>
2021-08-23 12:13:44 -0400

    tv_lqr initial revision draft.

--- irs_lqr/tv_lqr.py
+++ irs_lqr/tv_lqr.py
@@ -27,7 +27,8 @@ def get_solver(solver_name: str):
     raise ValueError("Do not recognize solver.")
 
 
-def solve_tvlqr(At, Bt, ct, Q, Qd, R, x0, xdt, xbound, ubound, solver,
+def solve_tvlqr(At, Bt, ct, Q, Qd, R, x0, x_trj_d, solver, indices_u_into_x=None,
+                xbound_abs=None, ubound_abs=None, xbound_rel=None, ubound_rel=None,
                 xinit=None, uinit=None):
     """
     Solve time-varying LQR problem as an instance of a quadratic program (QP).
@@ -41,9 +42,21 @@ def solve_tvlqr(At, Bt, ct, Q, Qd, R, x0, xdt, xbound, ubound, solver,
      - Qd    (np.array, dim: n x n): Quadratic cost on final state error x(T) - xd(T)     
      - R    (np.array, dim: m x m): Quadratic cost on actuation.
      - x0   (np.array, dim: n): Initial state of the problem.
-     - xdt  (np.array, dim: (T + 1) x n): Desired trajectory of the system.
-     - xbound (np.array, dim: 2 x n): (lb, ub) Bound on state variables.
-     - ubound (np.array, dim: 2 x u): (lb, ub) Bound on input variables.
+     - x_trj_d  (np.array, dim: (T + 1) x n): Desired trajectory of the system.
+     - solver (Drake's solver class): solver. Initialized outside the loop for 
+             better performance.     
+     - indices_u_into_x: (np.array): in a quasistatic system, x is the configuration
+         of the whole system, whereas u is the commanded configuration of the 
+         actuated DOFs. This is used to detect whether or not the system is using
+         position-controlled dynamics, or force/velocity controlled dynamics.
+     - xbound_abs (np.array, dim: 2 x n): (lb, ub) Bound on state variables (abs).
+         constrains x globally.
+     - ubound_abs (np.array, dim: 2 x u): (lb, ub) Bound on input variables (abs).
+         constrains u globally.
+     - xbound_rel (np.array, dim: 2 x n): (lb, ub) Bound on state variables (rel).
+         constrains the of x relative to the last x (Bound on x[t] - x[t-1]).
+     - ubound_rel (np.array, dim: 2 x u): (lb, ub) Bound on input variables (rel).
+         constrains the of u relative to the last u (Bound on u[t] - u[t-1]).
      - solver (Drake's solver class): solver. Initialized outside the loop for 
              better performance.
      - xinit (np.array, dim: (T + 1) x n): initial guess for state.
@@ -54,42 +67,72 @@ def solve_tvlqr(At, Bt, ct, Q, Qd, R, x0, xdt, xbound, ubound, solver,
 
     prog = MathematicalProgram()
 
-    timesteps = At.shape[0]
-    state_dim = Q.shape[0]
-    input_dim = R.shape[0]
+    T = At.shape[0]
+    n_x = Q.shape[0]
+    n_u = R.shape[0]
 
     # 1. Declare new variables corresponding to optimal state and input.
-    xt = prog.NewContinuousVariables(timesteps + 1, state_dim, "state")
-    ut = prog.NewContinuousVariables(timesteps, input_dim, "input")
+    xt = prog.NewContinuousVariables(T + 1, n_x, "state")
+    ut = prog.NewContinuousVariables(T, n_u, "input")
+    dxt = prog.NewContinuousVariables(T, n_u, "delta_state")
+    dut = prog.NewContinuousVariables(T, n_u, "delta_input")
 
     if xinit is not None:
         prog.SetInitialGuess(xt, xinit)
     if uinit is not None:
         prog.SetInitialGuess(ut, uinit)
+
     # 2. Initial constraint.
     prog.AddConstraint(eq(xt[0, :], x0))
 
     # 3. Loop over to add dynamics constraints and costs.
-    for t in range(timesteps):
+    for t in range(T):
         # Add affine dynamics constraint.
         prog.AddLinearEqualityConstraint(
-            np.hstack((At[t], Bt[t], -np.eye(state_dim))), -ct[t],
+            np.hstack((At[t], Bt[t], -np.eye(n_x))), -ct[t],
             np.hstack((xt[t, :], ut[t, :], xt[t + 1, :]))
         )
 
-        prog.AddBoundingBoxConstraint(xbound[0], xbound[1], xt[t, :])
-        prog.AddBoundingBoxConstraint(ubound[0], ubound[1], ut[t, :])
+        # Compute differences. 
+        if t == 0:
+            du = ut[t] - xt[t, indices_u_into_x]
+        else:
+            du = ut[t] - ut[t - 1]
+        dx = xt[t + 1] - xt[t]
+
+        prog.AddConstraint(eq(du, dut[t]))
+        prog.AddConstraint(eq(dx, dxt[t]))
+
+        # Add constraints.
+        if xbound_abs is not None:
+            prog.AddBoundingBoxConstraint(
+                xbound_abs[0, t], xbound_abs[1, t], xt[t, :])
+        if ubound_abs is not None:
+            prog.AddBoundingBoxConstraint(
+                ubound_abs[0, t], ubound_abs[1, t], ut[t, :])
+        if xbound_rel is not None:
+            prog.AddBoundingBoxConstraint(
+                xbound_rel[0, t], xbound_rel[1, t], dxt[t, :])
+        if ubound_rel is not None:
+            prog.AddBoundingBoxConstraint(
+                ubound_rel[0, t], ubound_rel[1, t], dut[t, :])
 
         # Add cost.
-        prog.AddQuadraticErrorCost(Q, xdt[t, :], xt[t, :])
-        prog.AddQuadraticCost(R, np.zeros(input_dim), ut[t, :])
+        prog.AddQuadraticErrorCost(Q, x_trj_d[t, :], xt[t, :])
+
+        if indices_u_into_x is not None:
+            prog.AddQuadraticCost(du.dot(R).dot(du))
+        else:
+            prog.AddQuadraticCost(R, np.zeros(n_u), ut[t, :])
 
     # Add final constraint.
-    prog.AddQuadraticErrorCost(Qd, xdt[timesteps, :], xt[timesteps, :])
-    # prog.AddBoundingBoxConstraint(xbound[0], xbound[1], xt[timesteps,:])
+    prog.AddQuadraticErrorCost(Qd, x_trj_d[T, :], xt[T, :])
+    
+    if xbound_abs is not None:
+        prog.AddBoundingBoxConstraint(
+            xbound_abs[0, T], xbound_abs[1, T], xt[T, :])
 
     # 4. Solve the program.
-
     result = solver.Solve(prog)
 
     if not result.is_success():
@@ -99,7 +142,6 @@ def solve_tvlqr(At, Bt, ct, Q, Qd, R, x0, xdt, xbound, ubound, solver,
     ut_star = result.GetSolution(ut)
 
     return xt_star, ut_star
-
 
 def solve_tvlqr_quasistatic(
         At: np.ndarray, Bt: np.ndarray, ct: np.ndarray, Q: np.ndarray,
