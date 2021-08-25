5ca3df77f992c6c5afce93fda7f21bc96cf6ce3d
hjsuh94 <hjsuh94@gmail.com>
2021-08-25 11:42:03 -0400

    clean up implementation of mbp dynamics

--- irs_lqr/mbp_dynamics.py
+++ irs_lqr/mbp_dynamics.py
@@ -13,19 +13,31 @@ from irs_lqr.dynamical_system import DynamicalSystem
 
 class MbpDynamics(QuasistaticDynamics):
     def __init__(self, h: float, q_sim_py: QuasistaticSimulator,
+                 q_sim_ad: QuasistaticSimulator,
                  q_sim: QuasistaticSimulatorCpp):
         super().__init__(h, q_sim_py, q_sim)
+        """
+        Prerequisites for using MBP dynamics.
+        1. q_sim_py has internal_vis to True.
+        2. q_sim_py_ad has internal_vis to False, since Meshcat cannot be 
+           autodiffed as a diagram.
+        """
 
+        # Since this is a second order sim, the velocities are also included
+        # in the state of the system.
         self.dim_x = self.plant.num_positions() + self.plant.num_velocities()
-
-        # Current only support three dimensional systems.
+        # Currently only support two dimensional systems.
         assert(self.plant.num_positions() == self.plant.num_velocities())
 
+        
         # Get diagram and copy to AutodiffXd.
         self.diagram = self.q_sim_py.diagram
-        self.diagram_ad = self.diagram.ToAutoDiffXd()
+        # q_sim_ad can be autodiffed since it is not connected to Meshcat.
+        self.q_sim_ad = q_sim_ad
+        self.diagram_ad = self.q_sim_ad.diagram
+        self.diagram_ad = self.diagram_ad.ToAutoDiffXd()
 
-        # Get plant and scene graph.
+        # Get plant and scene graph as well as their autodiff components.
         self.plant = self.q_sim_py.get_plant()
         self.scene_graph = self.q_sim_py.get_scene_graph()
 
@@ -53,10 +65,14 @@ class MbpDynamics(QuasistaticDynamics):
         self.simulator_ad = Simulator_[AutoDiffXd](self.diagram_ad,
             self.context_ad)
 
+        # Set up initial time for the simulators.
         self.simulator_time = 0.0
         self.simulator_time_ad = 0.0
 
-    def get_x_from_q_dict(self, q_dict: Dict[ModelInstanceIndex, np.ndarray]):
+    def get_x_from_qv_dict(self, q_dict: Dict[ModelInstanceIndex, np.ndarray]):
+        """
+        Current assumes len(positions) == len(velocities)
+        """
         x = np.zeros(self.plant.num_positions())
         v = np.zeros(self.plant.num_velocities())
         for model, n_q_indices in self.position_indices.items():
@@ -65,23 +81,38 @@ class MbpDynamics(QuasistaticDynamics):
             v[n_q_indices] = q_dict[model][len(n_q_indices):]
         return np.hstack((x,v))
 
-    def get_q_dict_from_x(self, x: np.ndarray):
+    def get_qv_dict_from_x(self, x: np.ndarray):
+        """
+        Current assumes len(positions) == len(velocities)
+        """        
         offset = self.plant.num_velocities()
-        q_dict = {
+        qv_dict = {
             model: np.hstack((x[n_q_indices], x[np.array(n_q_indices) + offset]))
             for model, n_q_indices in self.position_indices.items()}
-        return q_dict
+        return qv_dict
 
-    def update_mbp_positions_and_velocities(self, plant, plant_context, 
-        scene_graph, scene_graph_context, q_dict):
+    def publish_trajectory(self, x_traj):
+        q_dict_traj = [self.get_q_dict_from_x(x) for x in x_traj]
+        self.q_sim_py.animate_system_trajectory(h=self.h,
+                                                q_dict_traj=q_dict_traj)
 
-        for model_instance_idx, q in q_dict.items():
+    def update_mbp_positions_and_velocities_from_dict(
+        self, plant, plant_context, 
+        scene_graph, scene_graph_context, qv_dict):
+
+        for model_instance_idx, qv in qv_dict.items():
             plant.SetPositionsAndVelocities(
                 plant_context,
                 plant.GetModelInstanceByName(
-                    self.plant.GetModelInstanceName(model_instance_idx)), q)
+                    self.plant.GetModelInstanceName(model_instance_idx)), qv)
 
+        # Update query object.
+        self.query_object = scene_graph.get_query_output_port().Eval(
+                scene_graph_context)
 
+    def update_mbp_positions_and_velocities_from_vec(self, plant, plant_context,
+        scene_graph, scene_graph_context, qv_vec):
+        plant.SetPositionsAndVelocities(plant_context, qv_vec)
         # Update query object.
         self.query_object = scene_graph.get_query_output_port().Eval(
                 scene_graph_context)
@@ -106,14 +137,6 @@ class MbpDynamics(QuasistaticDynamics):
             R[i_start: i_start + n_v_i, i_start: i_start + n_v_i] = \
                 R_dict[model]
         return R
-
-
-    def update_mbp_positions_and_velocities_xv(self, plant, plant_context,
-        scene_graph, scene_graph_context, xv):
-        plant.SetPositionsAndVelocities(plant_context, xv)
-        # Update query object.
-        self.query_object = scene_graph.get_query_output_port().Eval(
-                scene_graph_context)
         
     def update_mbp_inputs(self, plant, plant_context, q_a_dict):
         for model in self.models_actuated:
@@ -127,17 +150,16 @@ class MbpDynamics(QuasistaticDynamics):
         :param u: commanded positions of models in
             self.q_sim.models_actuated, concatenated into one vector.
         """
-        q_dict = self.get_q_dict_from_x(x)
+        qv_dict = self.get_qv_dict_from_x(x)
         q_a_cmd_dict = self.get_q_a_cmd_dict_from_u(u)
 
-        self.update_mbp_positions_and_velocities(
+        self.update_mbp_positions_and_velocities_from_dict(
             self.plant, self.context_plant,
-            self.scene_graph, self.context_sg, q_dict)
+            self.scene_graph, self.context_sg, qv_dict)
         self.update_mbp_inputs(self.plant, self.context_plant,
             q_a_cmd_dict)
 
         self.simulator_time += self.h
-
         self.simulator.AdvanceTo(self.simulator_time)
 
         x_next = self.plant.GetPositionsAndVelocities(self.context_plant)
@@ -150,17 +172,16 @@ class MbpDynamics(QuasistaticDynamics):
         :param u: commanded positions of models in
             self.q_sim.models_actuated, concatenated into one vector.
         """
-        q_dict = self.get_q_dict_from_x(x)
+        q_dict = self.get_qv_dict_from_x(x)
         q_a_cmd_dict = self.get_q_a_cmd_dict_from_u(u)
 
-        self.update_mbp_positions_and_velocities(
+        self.update_mbp_positions_and_velocities_from_dict(
             self.plant, self.context_plant,
             self.scene_graph, self.context_sg, q_dict)
         self.update_mbp_inputs(self.plant, self.context_plant,
             q_a_cmd_dict)
 
         self.simulator_time += self.h
-
         self.simulator.AdvanceTo(self.simulator_time)
 
         x_next = self.plant.GetPositionsAndVelocities(self.context_plant)
@@ -190,11 +211,7 @@ class MbpDynamics(QuasistaticDynamics):
 
         q_a_cmd_dict = self.get_q_a_cmd_dict_from_u(u_ad)
 
-        """self.update_mbp_positions_and_velocities(
-            self.plant_ad, self.context_plant_ad,
-            self.scene_graph_ad, self.context_sg_ad, q_dict)
-            """
-        self.update_mbp_positions_and_velocities_xv(
+        self.update_mbp_positions_and_velocities_from_vec(
             self.plant_ad, self.context_plant_ad,
             self.scene_graph_ad, self.context_sg_ad, x_ad)
         self.update_mbp_inputs(self.plant_ad, self.context_plant_ad,
