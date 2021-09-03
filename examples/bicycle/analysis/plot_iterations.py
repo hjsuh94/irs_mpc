08b9054c95eeb4590c38614d5707f13d877a6c22
Pang <pangtao@mit.edu>
2021-09-02 20:45:12 -0400

    format figures

--- examples/bicycle/analysis/plot_iterations.py
+++ examples/bicycle/analysis/plot_iterations.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 12})
+from matplotlib import rc
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+rc('text', usetex=True)
 
-## Easy
+#%% Easy
 folder_name = "bicycle"
 
 result_exact = np.loadtxt(
@@ -14,8 +18,7 @@ result_zero = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_easy_zero.csv",
     delimiter=",")
 
-plt.figure()
-
+plt.figure(figsize=(4, 3), dpi=200)
 plt.plot(result_exact, marker='x', color='red', label="exact")
 plt.plot(result_first, marker='v', color='springgreen', label="first order")
 plt.plot(result_zero, marker='^', color='blue', label="zero order")
@@ -25,8 +28,10 @@ plt.xlabel('iterations')
 plt.ylabel('Cost (log scale)')
 plt.title("Dubin's Car (Easy)")
 plt.grid()
+plt.savefig("dubin_car_easy.pdf", bbox_inches='tight', pad_inches=0.01)
 plt.show()
 
+#%%
 result_exact = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_hard_exact.csv",
     delimiter=",")
@@ -37,8 +42,7 @@ result_zero = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_hard_zero.csv",
     delimiter=",")
 
-plt.figure()
-
+plt.figure(figsize=(4, 3), dpi=200)
 plt.plot(result_exact, marker='x', color='red', label="exact")
 plt.plot(result_first, marker='v', color='springgreen', label="first order")
 plt.plot(result_zero, marker='^', color='blue', label="zero order")
@@ -48,5 +52,6 @@ plt.xlabel('iterations')
 plt.ylabel('Cost (logscale)')
 plt.title("Dubin's Car (Hard)")
 plt.grid()
+plt.savefig("dubin_car_hard.pdf", bbox_inches='tight', pad_inches=0.01)
 plt.show()
 
