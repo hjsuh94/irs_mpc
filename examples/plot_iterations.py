22493721cdbe09038850f2c978951fe6bfc43a5a
Pang <pangtao@mit.edu>
2021-09-02 23:00:17 -0400

    better looking plots

--- examples/plot_iterations.py
+++ examples/plot_iterations.py
@@ -1,31 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 plt.rcParams.update({'font.size': 12})
 from matplotlib import rc
+
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 rc('text', usetex=True)
 
-plt.figure(figsize=(8, 4), dpi=200)
+fig, axes = plt.subplots(2, 4, figsize=(16, 5.1), dpi=300)
+title_font_size = 12
 
-#%% Pendulum
-
+# %% Pendulum
 result_exact = np.load("examples/pendulum/analysis/exact_cost.npy")
 result_first = np.load("examples/pendulum/analysis/first_order_cost.npy")
 result_zero = np.load("examples/pendulum/analysis/zero_order_cost.npy")
 
-plt.subplot(4,2,1)
-plt.plot(result_exact, marker='x',color='red', label="exact")
-plt.plot(result_first, marker='v', color='springgreen', label="first order")
-plt.plot(result_zero, marker='^', color='blue', label="zero order")
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost')
-plt.title('Pendulum')
-plt.grid()
-plt.show()
+ax = axes[0, 0]
+ax.plot(result_exact, marker='x', color='red', label="exact")
+ax.plot(result_first, marker='v', color='springgreen', label="first order")
+ax.plot(result_zero, marker='^', color='blue', label="zero order")
+ax.legend()
+# ax.set_xlabel('iterations')
+ax.set_ylabel('Cost')
+ax.set_title('Pendulum', fontsize=title_font_size)
+ax.grid()
 
-
-#%% Easy
+# %% Easy
 folder_name = "bicycle"
 
 result_exact = np.loadtxt(
@@ -38,18 +38,18 @@ result_zero = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_easy_zero.csv",
     delimiter=",")
 
-plt.subplot(4,2,2)
-plt.plot(result_exact, marker='x', color='red', label="exact")
-plt.plot(result_first, marker='v', color='springgreen', label="first order")
-plt.plot(result_zero, marker='^', color='blue', label="zero order")
-plt.yscale('log')
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost (log scale)')
-plt.title("Dubin's Car (Easy)")
-plt.grid()
+ax = axes[0, 1]
+ax.plot(result_exact, marker='x', color='red', label="exact")
+ax.plot(result_first, marker='v', color='springgreen', label="first order")
+ax.plot(result_zero, marker='^', color='blue', label="zero order")
+ax.set_yscale('log')
+ax.legend()
+# ax.set_xlabel('iterations')
+# ax.set_ylabel('Cost (log scale)')
+ax.set_title("Dubin's Car (Easy)", fontsize=title_font_size)
+ax.grid()
 
-#%% Hard
+# %% Hard
 result_exact = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_hard_exact.csv",
     delimiter=",")
@@ -60,19 +60,18 @@ result_zero = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_hard_zero.csv",
     delimiter=",")
 
-plt.subplot(4,2,3)
-plt.plot(result_exact, marker='x', color='red', label="exact")
-plt.plot(result_first, marker='v', color='springgreen', label="first order")
-plt.plot(result_zero, marker='^', color='blue', label="zero order")
-plt.yscale('log')
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost (logscale)')
-plt.title("Dubin's Car (Hard)")
-plt.grid()
+ax = axes[0, 2]
+ax.plot(result_exact, marker='x', color='red', label="exact")
+ax.plot(result_first, marker='v', color='springgreen', label="first order")
+ax.plot(result_zero, marker='^', color='blue', label="zero order")
+ax.set_yscale('log')
+ax.legend()
+# ax.set_xlabel('iterations')
+# ax.set_ylabel('Cost (logscale)')
+ax.set_title("Dubin's Car (Hard)", fontsize=title_font_size)
+ax.grid()
 
-#%% Quadrotor
-
+# %% Quadrotor
 folder_name = "quadrotor"
 
 result_exact = np.loadtxt(
@@ -85,20 +84,18 @@ result_zero = np.loadtxt(
     "examples/" + folder_name + "/analysis/" + folder_name + "_zero.csv",
     delimiter=",")
 
-plt.subplot(4,2,4)
-plt.plot(result_exact, marker='x', color='red', label="exact")
-plt.plot(result_first, marker='v', color='springgreen', label="first order")
-plt.plot(result_zero, marker='^', color='blue', label="zero order")
-plt.yscale('log')
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost (log scale)')
-plt.title("Quadrotor")
-plt.grid()
+ax = axes[0, 3]
+ax.plot(result_exact, marker='x', color='red', label="exact")
+ax.plot(result_first, marker='v', color='springgreen', label="first order")
+ax.plot(result_zero, marker='^', color='blue', label="zero order")
+ax.set_yscale('log')
+ax.legend()
+# ax.set_xlabel('iterations')
+# ax.set_ylabel('Cost (log scale)')
+ax.set_title("Quadrotor", fontsize=title_font_size)
+ax.grid()
 
-#%% Planar Hand 1
-
-
+# %% Planar Hand 1
 exact = np.loadtxt(
     "examples/planar_hand/analysis/planar_hand_exact.csv",
     delimiter=",")
@@ -110,20 +107,20 @@ zero_order_B = np.loadtxt(
     delimiter=",")
 zero_order_AB = np.loadtxt(
     "examples/planar_hand/analysis/planar_hand_zero_order_AB.csv",
-    delimiter=",")    
+    delimiter=",")
 
-plt.subplot(4,2,5)
-plt.plot(exact, marker='x', color='red', label='exact')
-plt.plot(first_order, marker='v', color='springgreen', label='first order')
-plt.plot(zero_order_AB, marker='^', color='blue', label='zero order')
-#plt.plot(zero_order_AB, marker='+', color='magenta', label='zero order_AB')
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost')
-plt.title("Planar Hand (Move Right)")
-plt.grid()
+ax = axes[1, 0]
+ax.plot(exact, marker='x', color='red', label='exact')
+ax.plot(first_order, marker='v', color='springgreen', label='first order')
+ax.plot(zero_order_AB, marker='^', color='blue', label='zero order')
+# ax.plot(zero_order_AB, marker='+', color='magenta', label='zero order_AB')
+ax.legend()
+ax.set_xlabel('iterations')
+ax.set_ylabel('Cost')
+ax.set_title("Planar Hand (Move Right)", fontsize=title_font_size)
+ax.grid()
 
-#%% Planar Hand 2
+# %% Planar Hand 2
 
 exact = np.loadtxt(
     "examples/planar_hand/analysis/planar_hand_spin_second_exact.csv",
@@ -135,19 +132,18 @@ zero_order = np.loadtxt(
     "examples/planar_hand/analysis/planar_hand_spin_second_zero.csv",
     delimiter=",")
 
-plt.subplot(4,2,6)
-plt.plot(exact, marker='x', color='red', label='exact')
-plt.plot(first_order, marker='v', color='springgreen', label='first order')
-plt.plot(zero_order, marker='^', color='blue', label='zero order')
-plt.legend()
-plt.yscale('log')
-plt.xlabel('iterations')
-plt.ylabel('Cost (log scale)')
-plt.title("Planar Hand (Spin In-hand, Second-Order Sim)")
-plt.grid()
+ax = axes[1, 1]
+ax.plot(exact, marker='x', color='red', label='exact')
+ax.plot(first_order, marker='v', color='springgreen', label='first order')
+ax.plot(zero_order, marker='^', color='blue', label='zero order')
+ax.legend()
+ax.set_yscale('log')
+ax.set_xlabel('iterations')
+# ax.set_ylabel('Cost (log scale)')
+ax.set_title("Planar Hand (Spin In-hand, Second-Order Sim)", fontsize=title_font_size)
+ax.grid()
 
-#%% Box Pushing
-
+# %% Box Pushing
 exact = np.loadtxt(
     "examples/box_pushing/analysis/box_pushing_exact.csv",
     delimiter=",")
@@ -158,17 +154,17 @@ zero_order = np.loadtxt(
     "examples/box_pushing/analysis/box_pushing_zero_order_AB.csv",
     delimiter=",")
 
-plt.subplot(4,2,7)
-plt.plot(exact[0:-2], marker='x', color='red', label='exact')
-plt.plot(first_order[0:-2], marker='v', color='springgreen', label='first order')
-plt.plot(zero_order[0:-2], marker='^', color='blue', label='zero order')
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost')
-plt.title("Planar Pushing")
-plt.grid()
+ax = axes[1, 2]
+ax.plot(exact[0:-2], marker='x', color='red', label='exact')
+ax.plot(first_order[0:-2], marker='v', color='springgreen', label='first order')
+ax.plot(zero_order[0:-2], marker='^', color='blue', label='zero order')
+ax.legend()
+ax.set_xlabel('iterations')
+# ax.set_ylabel('Cost')
+ax.set_title("Planar Pushing", fontsize=title_font_size)
+ax.grid()
 
-#%% Box Pivoting
+# %% Box Pivoting
 
 exact = np.loadtxt(
     "examples/box_pivoting/analysis/box_pivoting_exact2.csv",
@@ -180,15 +176,17 @@ zero_order = np.loadtxt(
     "examples/box_pivoting/analysis/box_pivoting_zero.csv",
     delimiter=",")
 
-plt.subplot(4,2,8)
-plt.plot(exact[0:6], marker='x', color='red', label='exact')
-plt.plot(first_order[0:6], marker='v', color='springgreen', label='first order')
-plt.plot(zero_order[0:6], marker='^', color='blue', label='zero order')
-plt.legend()
-plt.xlabel('iterations')
-plt.ylabel('Cost')
-plt.title("Box Pivoting")
-plt.grid()
+ax = axes[1, 3]
+ax.plot(exact[0:6], marker='x', color='red', label='exact')
+ax.plot(first_order[0:6], marker='v', color='springgreen', label='first order')
+ax.plot(zero_order[0:6], marker='^', color='blue', label='zero order')
+ax.legend()
+ax.set_xlabel('iterations')
+# ax.set_ylabel('Cost')
+ax.set_title("Box Pivoting", fontsize=title_font_size)
+ax.grid()
 
-#%%
+# %%
+plt.tight_layout()
 plt.savefig("results.pdf", bbox_inches='tight', pad_inches=0.01)
+plt.show()
