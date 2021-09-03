58d98b6ca9e9fbfe57601439c57da53469913c44
Pang <pangtao@mit.edu>
2021-09-03 13:26:01 -0400

    Better figure for examples.

--- examples/box_pushing/analysis/box_on_box.py
+++ examples/box_pushing/analysis/box_on_box.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.rc('font', size=12) 
+plt.rcParams.update({'font.size': 12})
+from matplotlib import rc
+
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+rc('text', usetex=True)
+
 
 def dynamics(x, u):
     m = 1
@@ -9,15 +14,16 @@ def dynamics(x, u):
     h = 0.1
     if (u > 1):
         w1 = m / (m + h ** 2.0 * k)
-        w2 = h ** 2.0 * k / (m + h **2.0 * k)
+        w2 = h ** 2.0 * k / (m + h ** 2.0 * k)
         return np.array([w1 * 1.0 + w2 * u, w1 * 1.0 + w2 * u])
     else:
         return np.array([u, 1.0])
 
+
 w_lst = np.random.normal(0, 1.0, 1000)
 u_lst = np.linspace(-2, 2, 1000)
-xnext_lst = np.zeros((1000,2))
-xtrue_lst = np.zeros((1000,2))
+xnext_lst = np.zeros((1000, 2))
+xtrue_lst = np.zeros((1000, 2))
 
 for i in range(len(u_lst)):
     bundle = np.array([0.0, 0.0])
@@ -27,27 +33,30 @@ for i in range(len(u_lst)):
     xnext_lst[i] = bundle
     xtrue_lst[i] = dynamics(0.0, u_lst[i])
 
-fig = plt.figure(figsize=(12,5))
-plt.subplot(1,2,1)
-plt.plot(u_lst, xnext_lst[:,1], 'r--', label='bundled dynamics')
-plt.plot(u_lst, xtrue_lst[:,1], 'r-', label='true dynamics')
+fig = plt.figure(figsize=(8, 3.2), dpi=200)
+plt.subplot(1, 2, 1)
+plt.plot(u_lst, xnext_lst[:, 1], 'r--', label='bundled dynamics')
+plt.plot(u_lst, xtrue_lst[:, 1], 'r-', label='true dynamics')
 plt.legend()
-plt.xlabel('u^1')
-plt.ylabel('x_{k+1}^2')
+plt.xlabel(r'$u_1$')
+plt.ylabel(r'$x_2^\prime$')
+
 
 def dynamics_anitescu(phi):
     if phi > 0.02:
         return 0
-    if phi <= 0.02 and phi > 0:
+    if 0.02 >= phi > 0:
         return -0.09 / 0.02 * phi + 0.09
     if phi < 0:
         return 0.09
+
 
 def dynamics_lcp(phi):
     if phi > 0:
         return 0
     else:
         return 0.09
+
 
 w_lst = np.random.normal(0, 0.01, 4000)
 phi_lst = np.linspace(0.0, 0.1, 1000)
@@ -59,9 +68,9 @@ lcp_bundle_lst = []
 
 for i in range(len(phi_lst)):
     anitescu_lst.append(dynamics_anitescu(phi_lst[i]))
-    lcp_lst.append(dynamics_lcp(phi_lst[i]))    
+    lcp_lst.append(dynamics_lcp(phi_lst[i]))
     bundle_anitescu = 0.0
-    bundle_lcp = 0.0    
+    bundle_lcp = 0.0
     for j in range(len(w_lst)):
         bundle_anitescu += dynamics_anitescu(phi_lst[i] + w_lst[j])
         bundle_lcp += dynamics_lcp(phi_lst[i] + w_lst[j])
@@ -69,23 +78,20 @@ for i in range(len(phi_lst)):
     bundle_lcp /= len(w_lst)
     anitescu_bundle_lst.append(bundle_anitescu)
     lcp_bundle_lst.append(bundle_lcp)
-    
 
-plt.subplot(1,2,2)
-plt.plot(np.array(lcp_lst), phi_lst, '-', color='blue', 
-    label='LCP Dynamics')
+plt.subplot(1, 2, 2)
+plt.plot(np.array(lcp_lst), phi_lst, '-', color='blue',
+         label='LCP Dynamics')
 plt.plot(np.array(lcp_bundle_lst), phi_lst, '--', color='blue',
-    label="Bundle Dynamics (LCP)")
+         label="Bundle Dynamics (LCP)")
 plt.plot(np.array(anitescu_lst), phi_lst, '-', color='springgreen',
-    label="Anitescu")
+         label="Anitescu")
 plt.plot(np.array(anitescu_bundle_lst), phi_lst, '--', color='springgreen',
-    label="Bundle Dynamics (Anitescu)")
+         label="Bundle Dynamics (Anitescu)")
 
-plt.xlabel('x_{k+1}^2')
-plt.ylabel('phi')
+plt.xlabel(r'$x_2^\prime$')
+plt.ylabel(r'$\phi$')
 plt.legend()
+plt.tight_layout()
+plt.savefig("bundle_dynamics.pdf", bbox_inches='tight', pad_inches=0.01)
 plt.show()
-
-image_format = "svg"
-image_name = "bundle_dynamics.svg"
-fig.savefig(image_name, format=image_format, dpi=1200)
