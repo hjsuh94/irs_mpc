07a5d9df2cc9199dc0c4d720f31a40ab3a4e6422
Pang <pangtao@mit.edu>
2021-11-16 19:53:07 -0500

    Setting A to [I_u, 0; 0, 0] seems best for rotation tasks?

--- examples/planar_hand/run_planar_hand.py
+++ examples/planar_hand/run_planar_hand.py
@@ -41,7 +41,7 @@ q_robot_r_traj = PiecewisePolynomial.ZeroOrderHold(
 q_a_traj_dict_str = {robot_l_name: q_robot_l_traj,
                      robot_r_name: q_robot_r_traj}
 
-q_u0 = np.array([0, 0.35, 0])
+q_u0 = np.array([0.0, 0.35, 0])
 
 q0_dict_str = {object_name: q_u0,
                robot_l_name: qa_l_knots[0],
@@ -116,7 +116,7 @@ q_sim_py.animate_system_trajectory(h, q_dict_traj)
 #%%
 params = IrsLqrQuasistaticParameters()
 params.Q_dict = {
-    idx_u: np.array([10, 10, 1e-3]),
+    idx_u: np.array([1e-3, 1e-3, 10]),
     idx_a_l: np.array([1e-3, 1e-3]),
     idx_a_r: np.array([1e-3, 1e-3])}
 params.Qd_dict = {model: Q_i * 100 for model, Q_i in params.Q_dict.items()}
@@ -124,7 +124,7 @@ params.R_dict = {
     idx_a_l: 5 * np.array([1, 1]),
     idx_a_r: 5 * np.array([1, 1])}
 
-xd_dict = {idx_u: q_u0 + np.array([0.3, -0.1, 0.0]),
+xd_dict = {idx_u: q_u0 + np.array([0.3, -0.1, 0.5]),
            idx_a_l: qa_l_knots[0],
            idx_a_r: qa_r_knots[0]}
 xd = q_dynamics.get_x_from_q_dict(xd_dict)
