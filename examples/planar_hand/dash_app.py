cfd8074b3cfc991e5194538ae45d6cf11af21a8e
Unknown <pangtao@mit.edu>
2021-11-30 02:57:34 -0500

    WIP: still needs to visualize goal and principal axes.

--- examples/planar_hand/dash_app.py
+++ examples/planar_hand/dash_app.py
@@ -2,78 +2,131 @@ import os
 import json
 import pickle
 
+import meshcat
 import numpy as np
 import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 
 from qsim.simulator import (QuasistaticSimulator, QuasistaticSimParameters)
-from planar_hand_setup import (model_directive_path,
+from qsim.system import cpp_params_from_py_params
+from quasistatic_simulator_py import (QuasistaticSimulatorCpp)
+from planar_hand_setup import (model_directive_path, h,
                                robot_stiffness_dict, object_sdf_dict,
                                robot_l_name, robot_r_name, object_name)
 
+from irs_lqr.quasistatic_dynamics import QuasistaticDynamics
 from rrt.utils import set_orthographic_camera_yz
 
-# %% meshcat
+
+
+# %% quasistatic dynamics
+sim_params = QuasistaticSimParameters()
 q_sim_py = QuasistaticSimulator(
     model_directive_path=model_directive_path,
     robot_stiffness_dict=robot_stiffness_dict,
     object_sdf_paths=object_sdf_dict,
-    sim_params=QuasistaticSimParameters(),
+    sim_params=sim_params,
     internal_vis=True)
 
-set_orthographic_camera_yz(q_sim_py.viz.vis)
+# construct C++ backend.
+sim_params_cpp = cpp_params_from_py_params(sim_params)
+q_sim_cpp = QuasistaticSimulatorCpp(
+    model_directive_path=model_directive_path,
+    robot_stiffness_str=robot_stiffness_dict,
+    object_sdf_paths=object_sdf_dict,
+    sim_params=sim_params_cpp)
+
+q_dynamics = QuasistaticDynamics(h=h, q_sim_py=q_sim_py, q_sim=q_sim_cpp)
 
 model_a_l = q_sim_py.plant.GetModelInstanceByName(robot_l_name)
 model_a_r = q_sim_py.plant.GetModelInstanceByName(robot_r_name)
 model_u = q_sim_py.plant.GetModelInstanceByName(object_name)
 
+# %% meshcat
+vis = q_sim_py.viz.vis
+set_orthographic_camera_yz(vis)
+
+# goal
+vis["goal/cylinder"].set_object(
+    meshcat.geometry.Cylinder(height=0.01, radius=0.25),
+    meshcat.geometry.MeshLambertMaterial(color=0xff22dd, reflectivity=0.8))
+
+
+
 # %% load data from disk and format data.
 file_names_prefix = ['du_', 'qa_l_', 'qa_r_', 'qu_']
 suffix = '_r0.2'
 
-data = []
+reachable_set_data = []
 for name in file_names_prefix:
     path = os.path.join('data', f'{name}{suffix}.pkl')
     with open(path, 'rb') as f:
-        data.append(pickle.load(f))
+        reachable_set_data.append(pickle.load(f))
 
-du, qa_l, qa_r, qu = data
+du, qa_l, qa_r, qu = reachable_set_data
 
+path = os.path.join('data', 'traj_opt_fingers_and_sphere_on_a_circle.pkl')
+with open(path, 'rb') as f:
+    trj_data = pickle.load(f)
 
-#%% prepare panda data frame
-df = pd.DataFrame(np.vstack([qu['1_step'], qu['multi_step']]),
-                  columns=['y', 'z', 'theta'])
-df['type'] = ['1_step'] * len(qu['1_step']) + ['multi_step'] * len(qu['multi_step'])
-
+q_u0 = np.array([0, 0.35, 0])
+dqu_goal = np.array([result['dqu_goal'] for result in trj_data])
 
 # %%
-layout = go.Layout(scene=dict(aspectmode='data'), height=1000)
-data_1_step = go.Scatter3d(x=qu['1_step'][:, 0],
+hovertemplate = (
+    '<i>y</i>: %{x:.4f}<br>' +
+    '<i>z</i>: %{y:.4f}<br>' +
+    '<i>theta</i>: %{z:.4f}')
+
+hovertemplate_reachability = (hovertemplate +
+                              '<br><i>cost</i>: %{marker.color:.4f}')
+
+plot_1_step = go.Scatter3d(x=qu['1_step'][:, 0],
                            y=qu['1_step'][:, 1],
                            z=qu['1_step'][:, 2],
+                           name='1_step',
                            mode='markers',
+                           hovertemplate=hovertemplate,
                            marker=dict(color=0x00ff00,
-                                       size=1.5,
-                                       sizemode='diameter'))
-data_multi = go.Scatter3d(x=qu['multi_step'][:, 0],
+                                       size=2))
+plot_multi = go.Scatter3d(x=qu['multi_step'][:, 0],
                           y=qu['multi_step'][:, 1],
                           z=qu['multi_step'][:, 2],
+                          name='multi_step',
                           mode='markers',
+                          hovertemplate=hovertemplate,
                           marker=dict(color=0x00ff00,
-                                      size=1.5,
-                                      sizemode='diameter'))
+                                      size=2))
 
-fig = go.Figure(data=[data_1_step, data_multi],
-                layout=layout)
+plot_trj = go.Scatter3d(
+    x=q_u0[0] + dqu_goal[:, 0],
+    y=q_u0[1] + dqu_goal[:, 1],
+    z=q_u0[2] + dqu_goal[:, 2],
+    name='reachability',
+    mode='markers',
+    hovertemplate=hovertemplate_reachability,
+    marker=dict(size=6,
+                color=[result['cost']['Qu_f'] for result in trj_data],
+                colorscale='jet',
+                showscale=True,
+                opacity=0.8))
+
+layout = go.Layout(autosize=True, height=1200,
+                   legend=dict(orientation="h"),
+                   margin=dict(l=0, r=0, b=0, t=0))
+fig = go.Figure(data=[plot_1_step, plot_multi, plot_trj], layout=layout)
+fig.update_layout(coloraxis_colorbar=dict(yanchor="top", x=3, ticks="outside"))
 fig.update_scenes(camera_projection_type='orthographic',
                   xaxis_title_text='y',
                   yaxis_title_text='z',
-                  zaxis_title_text='theta')
+                  zaxis_title_text='theta',
+                  aspectmode='manual',
+                  aspectratio=dict(x=1.0, y=1.0, z=1.0))
 
 # %%
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -89,14 +142,14 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(
             dcc.Graph(
-                id='basic-interactions',
+                id='reachable-sets',
                 figure=fig),
-            width={'size': 6, 'offset': 0, 'order': 0},
+            width={'size': 7, 'offset': 0, 'order': 0},
         ),
         dbc.Col(
             html.Iframe(src='http://127.0.0.1:7000/static/',
-                        height=1024, width=768),
-            width={'size': 6, 'offset': 0, 'order': 0},
+                        height=800, width=1200),
+            width={'size': 5, 'offset': 0, 'order': 0},
         )
     ]),
     dbc.Row([
@@ -150,62 +203,59 @@ app.layout = dbc.Container([
 
 @app.callback(
     Output('hover-data', 'children'),
-    Input('basic-interactions', 'hoverData'))
-def display_hover_data(hoverData):
+    Input('reachable-sets', 'hoverData'),
+    State('reachable-sets', 'figure'))
+def display_hover_data(hoverData, figure):
     if hoverData is None:
         return json.dumps(hoverData, indent=2)
     point = hoverData['points'][0]
-    if point['curveNumber'] == 1:
-        name = "multi_step"
-    elif point['curveNumber'] == 0:
-        name = "1_step"
-
+    idx_fig = point['curveNumber']
+    name = figure['data'][idx_fig]['name']
     idx = point["pointNumber"]
-    q_dict = {
-        model_u: qu[name][idx],
-        model_a_l: qa_l[name][idx],
-        model_a_r: qa_r[name][idx]}
 
-    q_sim_py.update_mbp_positions(q_dict)
-    q_sim_py.draw_current_configuration()
+    if name == 'reachability':
+        pass
+    else:
+        q_dict = {
+            model_u: qu[name][idx],
+            model_a_l: qa_l[name][idx],
+            model_a_r: qa_r[name][idx]}
+
+        q_sim_py.update_mbp_positions(q_dict)
+        q_sim_py.draw_current_configuration()
 
     return json.dumps(hoverData, indent=2)
 
 
 @app.callback(
     Output('click-data', 'children'),
-    Input('basic-interactions', 'clickData'))
-def display_click_data(clickData):
-    if clickData is None:
-        return json.dumps(clickData, indent=2)
-    point = clickData['points'][0]
-    if point['curveNumber'] == 1:
-        name = "1_step"
-    elif point['curveNumber'] == 0:
-        name = "multi_step"
-
+    Input('reachable-sets', 'clickData'),
+    State('reachable-sets', 'figure'))
+def display_click_data(click_data, figure):
+    click_data_json = json.dumps(click_data, indent=2)
+    if click_data is None:
+        return click_data_json
+    point = click_data['points'][0]
+    idx_fig = point['curveNumber']
+    name = figure['data'][idx_fig]['name']
     idx = point["pointNumber"]
-    q_dict = {
-        model_u: qu[name][idx],
-        model_a_l: qa_l[name][idx],
-        model_a_r: qa_r[name][idx]}
 
-    q_sim_py.update_mbp_positions(q_dict)
-    q_sim_py.draw_current_configuration()
+    if name == 'reachability':
+        q_dynamics.publish_trajectory(trj_data[idx]['x_trj'])
 
-    return json.dumps(clickData, indent=2)
+    return click_data_json
 
 
 @app.callback(
     Output('selected-data', 'children'),
-    Input('basic-interactions', 'selectedData'))
+    Input('reachable-sets', 'selectedData'))
 def display_selected_data(selectedData):
     return json.dumps(selectedData, indent=2)
 
 
 @app.callback(
     Output('relayout-data', 'children'),
-    Input('basic-interactions', 'relayoutData'))
+    Input('reachable-sets', 'relayoutData'))
 def display_relayout_data(relayoutData):
     return json.dumps(relayoutData, indent=2)
 
--- rrt/utils.py
+++ rrt/utils.py
@@ -5,7 +5,7 @@ import meshcat
 def set_orthographic_camera_yz(vis: meshcat.Visualizer) -> None:
     # use orthographic camera, show YZ plane.
     camera = meshcat.geometry.OrthographicCamera(
-        left=-0.5, right=0.5, bottom=-1, top=1, near=-1000, far=1000)
+        left=-1, right=0.5, bottom=-0.5, top=1, near=-1000, far=1000)
     vis['/Cameras/default/rotated'].set_object(camera)
     vis['/Cameras/default/rotated/<object>'].set_property(
         "position", [0, 0, 0])
